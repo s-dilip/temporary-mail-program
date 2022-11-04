1fbe559eaf990d95b6e0b81ba6a12944ae0e8e6c
Swastik Dilip <justsyd224@gmail.com>
2022-11-04 15:56:03 +0000

    Added getting domain

--- temp_mail_program.py
+++ temp_mail_program.py
@@ -1,2 +1,21 @@
-print("Lets Go")
-print("Hello World")
+
+
+import requests
+
+
+def retrieve_domain():
+
+    response = requests.get("https://api.mail.tm/domains")
+    json = response.json()
+
+    domain = json['hydra:member'][0]['domain']
+
+    return domain
+
+def main():
+
+    domain = retrieve_domain()
+    print(domain)
+
+main()
+
